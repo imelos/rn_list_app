a592429f442325469fae241679de750c9ab159f4
imelos <imelose@gmail.com>
2024-03-07 16:49:14 +0400

    +

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
-// import Navigation from '@src/navigation/Navigation';
+import Navigation from '@src/navigation/Navigation';
 // import {COLORS} from '@src/colors';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar animated={true} />
-      {/* <Navigation /> */}
-      <Text>ff</Text>
+      <Navigation />
     </SafeAreaView>
   );
 }
